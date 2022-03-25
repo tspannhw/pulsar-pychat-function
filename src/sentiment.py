610a8dc55a87263cdbb899c60d9556c306372836
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2022-03-25 13:41:32 -0400

    Update sentiment.py

--- src/sentiment.py
+++ src/sentiment.py
@@ -9,14 +9,14 @@ class Chat(Function):
         pass
 
     def process(self, input, context):
-        logger = context.get_logger()
-        logger.info("Message Content: {0}".format(input))
+        #logger = context.get_logger()
+        #logger.info("Message Content: {0}".format(input))
         msg_id = context.get_message_id()
 
         fields = json.loads(input)
         sid = SentimentIntensityAnalyzer()
         ss = sid.polarity_scores(fields["comment"])
-        logger.info("Polarity: {0}".format(ss['compound']))
+        #logger.info("Polarity: {0}".format(ss['compound']))
         sentimentVal = 'Neutral'
         if ss['compound'] == 0.00:
             sentimentVal = 'Neutral'
